cd8a1b2ef004aa125f5d3ea1975f6ed279b168ff
Ubuntu <ubuntu@ip-172-31-0-194.us-west-2.compute.internal>
2013-07-11 17:01:12 +0000

    addes fs declartion in web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
  var buf = new Buffer (fs.readFileSync('index.html'))
  response.send(buf.toString());
