b11d314d5080f6ffc34494d2394ecc5738c2b5fc
Ubuntu <ubuntu@ip-172-31-44-80.us-west-2.compute.internal>
2013-07-14 01:10:46 +0000

    url grader.js modifications

--- grader.js
+++ grader.js
@@ -20,12 +20,13 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var request = require('request');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "undefined";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +66,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <web page url>','Path to url of a page')
         .parse(process.argv);
+
+    if(program.url){
+	request.get(program.url).pipe(fs.createWriteStream('temp.html'));
+	program.file = 'temp.html';
+    }
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
